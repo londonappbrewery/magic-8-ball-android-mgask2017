d9ac5f6abc53c76fed1701b3fafcc1f77019a55c
mgask1 <mark.gaskell@gmail.com>
2017-12-07 10:35:45 +0000

    Project complete

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall); //Creates a variable of type ImageView links with 8ball image
+
+        Button myButton = (Button) findViewById(R.id.askbutton); //Creates var for button links to 'Ask Button'
+
+        final int[] ballArray = new int[]{ //Creates array for the magic 8 ball
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        myButton.setOnClickListener(new View.OnClickListener() { //Listener waits for someone to click the button
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random(); //Creates random number var
+                int number = randomNumberGenerator.nextInt(5); //max of 5 links with pics
+                int imageResourceId = ballArray[number]; //links image to ballArray
+                ballDisplay.setImageResource(imageResourceId); //displays random 8ball image out of 5
+            }
+        });
+
+
     }
 }
